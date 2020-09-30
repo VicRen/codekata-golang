539745bd353fd470dffb805c5db434f8642f116a
VicRen <vic.ren@me.com>
2020-09-30 08:23:31 +0800

    fizz buzz

---
+++ fizzbuzz/fizz_buzz_test.go
@@ -0,0 +1,51 @@
+package main
+
+import "testing"
+
+func TestFizzBuzz(t *testing.T) {
+	tt := []struct {
+		name  string
+		input int
+		want  string
+	}{
+		{
+			"case 1",
+			1,
+			"",
+		},
+		{
+			"case 3",
+			3,
+			"Fizz",
+		},
+		{
+			"case 5",
+			5,
+			"Buzz",
+		},
+		{
+			"case 6",
+			6,
+			"Fizz",
+		},
+		{
+			"case 10",
+			10,
+			"Buzz",
+		},
+		{
+			"case 15",
+			15,
+			"FizzBuzz",
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			got := FizzBuzz(tc.input)
+			if got != tc.want {
+				t.Errorf("FizzBuzz(%d)=%s, want %s", tc.input, got, tc.want)
+			}
+		})
+	}
+}
---
+++ fizzbuzz/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 1; i <= 100; i++ {
+		fb := FizzBuzz(i)
+		if fb == "" {
+			fmt.Println(i)
+			continue
+		}
+		fmt.Println(fb)
+	}
+}
+
+func FizzBuzz(n int) string {
+	if n%3 == 0 && n%5 == 0 {
+		return "FizzBuzz"
+	} else if n%3 == 0 {
+		return "Fizz"
+	} else if n%5 == 0 {
+		return "Buzz"
+	}
+	return ""
+}
